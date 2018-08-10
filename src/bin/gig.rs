2f6edbdea27d9059cc36f41f8bb451786d541e5a
Jack O'Connor <oconnor663@gmail.com>
2018-08-10 15:23:18 -0400

    force gig to zero its memory before hashing

--- src/bin/gig.rs
+++ src/bin/gig.rs
@@ -3,21 +3,27 @@ extern crate blake2b_simd;
 use std::time::Instant;
 
 fn main() {
-    let start_t = Instant::now();
-
+    let before = Instant::now();
     let input = vec![0; 1_000_000_000];
+    let alloc_time = Instant::now() - before;
+    println!("alloc {:?}", alloc_time);
 
-    let alloc_t = Instant::now();
+    let before = Instant::now();
+    for &x in input.iter() {
+        if x != 0 {
+            panic!();
+        }
+    }
+    let read_time = Instant::now() - before;
+    println!("read {:?}", read_time);
 
+    let before = Instant::now();
     let hash = blake2b_simd::blake2b(&input);
-
-    let hash_t = Instant::now();
+    let hash_time = Instant::now() - before;
+    println!("hash {:?}", hash_time);
 
     for b in &hash[..] {
         print!("{:x}", b);
     }
     println!();
-
-    println!("alloc {:?}", alloc_t - start_t);
-    println!("hash {:?}", hash_t - alloc_t);
 }
