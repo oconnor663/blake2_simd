f9014c9c0a584110f8bf22dd02e13e0091cefa16
Jack O'Connor <oconnor663@gmail.com>
2018-10-22 21:36:30 -0400

    execute the test vectors for blake2bp
    The test vectors for keyed BLAKE2bp are broken, because we haven't
    handled keying the way the ref implementation does. Unfortunately the
    spec is unclear on this.

--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -33,13 +33,40 @@ fn blake2b_vectors() {
     let mut count = 0u64;
     for case in TEST_CASES.iter() {
         if &case.hash == "blake2b" {
-            println!("case {}, input {:?} key {:?}", count, case.in_, case.key);
+            println!("case {}, input {:?}, key {:?}", count, case.in_, case.key);
             let input_bytes = hex::decode(&case.in_).unwrap();
             let output = if case.key.is_empty() {
                 blake2b_simd::blake2b(&input_bytes)
             } else {
                 let key_bytes = hex::decode(&case.key).unwrap();
                 blake2b_simd::Params::new()
+                    .key(&key_bytes)
+                    .to_state()
+                    .update(&input_bytes)
+                    .finalize()
+            };
+            assert_eq!(case.out, &*output.to_hex());
+            count += 1;
+        }
+    }
+
+    // Make sure we don't accidentally skip all the tests somehow. If the number of test vectors
+    // changes in the future, we'll need to update this count.
+    assert_eq!(512, count);
+}
+
+#[test]
+fn blake2bp_vectors() {
+    let mut count = 0u64;
+    for case in TEST_CASES.iter() {
+        if &case.hash == "blake2bp" {
+            println!("case {}, input {:?}, key {:?}", count, case.in_, case.key);
+            let input_bytes = hex::decode(&case.in_).unwrap();
+            let output = if case.key.is_empty() {
+                blake2b_simd::blake2bp::blake2bp(&input_bytes)
+            } else {
+                let key_bytes = hex::decode(&case.key).unwrap();
+                blake2b_simd::blake2bp::Params::new()
                     .key(&key_bytes)
                     .to_state()
                     .update(&input_bytes)
