70e396ca151d3faadba558ec59d4e3548722b78f
Jack O'Connor <oconnor663@gmail.com>
2018-10-24 14:47:28 -0400

    add BLAKE2bp and update4 to the cycles benchmark

--- benches/count_cycles/src/main.rs
+++ benches/count_cycles/src/main.rs
@@ -7,7 +7,7 @@ extern crate test;
 
 const TOTAL_BYTES_PER_TYPE: usize = 1 << 30; // 1 gigabyte
 
-fn compression_fn() -> (u64, usize) {
+fn blake2b_compression() -> (u64, usize) {
     const SIZE: usize = 128;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -24,7 +24,7 @@ fn compression_fn() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn compression_fn_4x() -> (u64, usize) {
+fn blake2b_compression_4x() -> (u64, usize) {
     const SIZE: usize = 4 * 128;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -49,20 +49,7 @@ fn compression_fn_4x() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn hash_one_block() -> (u64, usize) {
-    const SIZE: usize = 128;
-    let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
-    let mut total_ticks = 0;
-    for _ in 0..iterations {
-        let start = amd64_timer::ticks_modern();
-        test::black_box(&blake2b_simd::blake2b(&[0; SIZE]));
-        let end = amd64_timer::ticks_modern();
-        total_ticks += end - start;
-    }
-    (total_ticks, iterations * SIZE)
-}
-
-fn hash_one_mb() -> (u64, usize) {
+fn blake2b_one_mb() -> (u64, usize) {
     const SIZE: usize = 1_000_000;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -75,25 +62,7 @@ fn hash_one_mb() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn hash_one_mb_in_chunks() -> (u64, usize) {
-    const SIZE: usize = 1 << 20;
-    const CHUNK_SIZE: usize = 1024;
-    let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
-    let mut total_ticks = 0;
-    for _ in 0..iterations {
-        let start = amd64_timer::ticks_modern();
-        let mut state = blake2b_simd::State::new();
-        for _ in 0..(SIZE / CHUNK_SIZE) {
-            state.update(&[0; CHUNK_SIZE]);
-        }
-        test::black_box(&state.finalize());
-        let end = amd64_timer::ticks_modern();
-        total_ticks += end - start;
-    }
-    (total_ticks, iterations * SIZE)
-}
-
-fn hash_one_mb_blake2bp() -> (u64, usize) {
+fn blake2bp_one_mb() -> (u64, usize) {
     const SIZE: usize = 1_000_000;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -106,8 +75,8 @@ fn hash_one_mb_blake2bp() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn hash_one_mb_update4() -> (u64, usize) {
-    const SIZE: usize = 1_000_000;
+fn blake2b_update4_one_mb() -> (u64, usize) {
+    const SIZE: usize = 4_000_000;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
     for _ in 0..iterations {
@@ -138,7 +107,7 @@ fn hash_one_mb_update4() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn hash_one_mb_sha1() -> (u64, usize) {
+fn sha1_openssl_one_mb() -> (u64, usize) {
     const SIZE: usize = 1_000_000;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -154,7 +123,7 @@ fn hash_one_mb_sha1() -> (u64, usize) {
     (total_ticks, iterations * SIZE)
 }
 
-fn hash_one_mb_sha512() -> (u64, usize) {
+fn sha512_openssl_one_mb() -> (u64, usize) {
     const SIZE: usize = 1_000_000;
     let iterations = TOTAL_BYTES_PER_TYPE / SIZE;
     let mut total_ticks = 0;
@@ -173,15 +142,13 @@ fn hash_one_mb_sha512() -> (u64, usize) {
 fn main() {
     assert!(is_x86_feature_detected!("avx2"));
     let cases: &[(&str, fn() -> (u64, usize))] = &[
-        ("compress", compression_fn),
-        ("compress4x", compression_fn_4x),
-        ("one block", hash_one_block),
-        ("one mb", hash_one_mb),
-        ("one mb chunks", hash_one_mb_in_chunks),
-        ("one mb blake2bp", hash_one_mb_blake2bp),
-        ("one mb update4", hash_one_mb_update4),
-        ("one mb sha1", hash_one_mb_sha1),
-        ("one mb sha512", hash_one_mb_sha512),
+        ("BLAKE2b compression function", blake2b_compression),
+        ("BLAKE2b 4-way compression function", blake2b_compression_4x),
+        ("BLAKE2b 1 MB", blake2b_one_mb),
+        ("BLAKE2bp 1 MB", blake2bp_one_mb),
+        ("BLAKE2b update4 1 MB", blake2b_update4_one_mb),
+        ("SHA1 OpenSSL 1 MB", sha1_openssl_one_mb),
+        ("SHA512 OpenSSL 1 MB", sha512_openssl_one_mb),
     ];
 
     for &(name, f) in cases.iter() {
@@ -190,7 +157,7 @@ fn main() {
         // Loop for real.
         let (total_cycles, total_bytes) = f();
         println!(
-            "{:15} {:.3}",
+            "{:34}  {:.2}",
             name,
             total_cycles as f64 / total_bytes as f64
         );
