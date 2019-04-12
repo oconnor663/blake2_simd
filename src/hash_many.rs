9b60a6678ace5ee8f338ce5c86ef22b679078b8a
Jack O'Connor <oconnor663@gmail.com>
2019-04-12 13:20:00 -0400

    update_many

--- src/hash_many.rs
+++ src/hash_many.rs
@@ -2,6 +2,8 @@ use crate::guts::{self, u64x8, Finalize, Implementation, Job, Stride};
 use crate::state_words_to_bytes;
 use crate::Hash;
 use crate::Params;
+use crate::State;
+use crate::BLOCKBYTES;
 use arrayvec::ArrayVec;
 
 type JobsVec<'a, 'b> = ArrayVec<[Job<'a, 'b>; guts::MAX_DEGREE]>;
@@ -104,6 +106,40 @@ where
     hash_many_inner(jobs, imp, Stride::Normal);
 }
 
+pub fn update_many<'a, 'b, I>(pairs: I)
+where
+    'b: 'a,
+    I: IntoIterator<Item = (&'a mut State, &'b [u8])>,
+{
+    let imp = Implementation::detect();
+    let jobs = pairs.into_iter().flat_map(|(state, mut input)| {
+        // For each pair, if the State has some input in its buffer, try to
+        // finish that buffer. If there wasn't enough input to do that --
+        // or if the input was empty to begin with -- skip this pair.
+        state.compress_buffer_if_possible(&mut input);
+        if input.is_empty() {
+            return None;
+        }
+        // Now we know the buffer is empty and there's more input. Make sure we
+        // buffer the final block, because update() doesn't finalize.
+        let mut last_block_start = input.len() - 1;
+        last_block_start -= last_block_start % BLOCKBYTES;
+        let (blocks, last_block) = input.split_at(last_block_start);
+        state.buf[..last_block.len()].copy_from_slice(last_block);
+        state.buflen = last_block.len() as u8;
+        // Finally, if the full blocks slice is non-empty, prepare that job for
+        // compression, and bump the State count.
+        if blocks.is_empty() {
+            None
+        } else {
+            let count = state.count;
+            state.count = state.count.wrapping_add(blocks.len() as u128);
+            Some(Job::new(&mut state.words, count, blocks, Finalize::NotYet))
+        }
+    });
+    hash_many_inner(jobs, imp, Stride::Normal);
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -147,6 +183,50 @@ mod test {
             for i in 0..LEN {
                 let expected = params[i].to_state().update(inputs[i]).finalize();
                 assert_eq!(expected, jobs[i].to_hash());
+            }
+        }
+    }
+
+    #[test]
+    fn test_update_many() {
+        // Use a length of inputs that will exercise all of the power-of-two loops.
+        const LEN: usize = 2 * guts::MAX_DEGREE - 1;
+
+        // Rerun LEN inputs LEN different times, with the empty input starting in a
+        // different spot each time.
+        let mut input = [0; LEN * BLOCKBYTES];
+        paint_test_input(&mut input);
+        for start_offset in 0..LEN {
+            let mut inputs: [&[u8]; LEN] = [&[]; LEN];
+            for i in 0..LEN {
+                let chunks = (i + start_offset) % LEN;
+                inputs[i] = &input[..chunks * BLOCKBYTES];
+            }
+
+            let mut params: ArrayVec<[Params; LEN]> = ArrayVec::new();
+            for i in 0..LEN {
+                let mut param = Params::new();
+                param.node_offset(i as u64);
+                param.last_node(i % 2 == 1);
+                params.push(param);
+            }
+
+            let mut states: ArrayVec<[State; LEN]> = ArrayVec::new();
+            for i in 0..LEN {
+                states.push(params[i].to_state());
+            }
+
+            // Run each input twice through, to exercise buffering.
+            update_many(states.iter_mut().zip(inputs.iter().cloned()));
+            update_many(states.iter_mut().zip(inputs.iter().cloned()));
+
+            // Check the outputs.
+            for i in 0..LEN {
+                let mut reference_state = params[i].to_state();
+                // Again, run the input twice.
+                reference_state.update(inputs[i]);
+                reference_state.update(inputs[i]);
+                assert_eq!(reference_state.finalize(), states[i].finalize());
             }
         }
     }
