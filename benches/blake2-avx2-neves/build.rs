38a2ba7d2436283b8d0753fc052fe57e592a00b6
Jack O'Connor <oconnor663@gmail.com>
2019-04-27 15:01:08 -0400

    make a note of the PERMUTE_WITH_GATHER option

--- benches/blake2-avx2-neves/build.rs
+++ benches/blake2-avx2-neves/build.rs
@@ -3,9 +3,13 @@ fn main() {
         .file("./blake2-avx2/blake2b.c")
         .file("./blake2-avx2/blake2bp.c")
         .file("./blake2-avx2/blake2sp.c")
-        // GCC and Clang
+        // Enable AVX2 for GCC and Clang.
         .flag_if_supported("-mavx2")
-        // MSVC
+        // Enable AVX2 for MSVC
         .flag_if_supported("/arch:AVX2")
+        // The implementation includes two different input loading strategies.
+        // Defining this variable enables the alternative, but in my testing
+        // it's slower than the default.
+        // .define("PERMUTE_WITH_GATHER", "1")
         .compile("blake2-avx2");
 }
