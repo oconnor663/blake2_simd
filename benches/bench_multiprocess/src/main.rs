9d9d49aa8701dff09a8cec411b7961486e30c3fe
Jack O'Connor <oconnor663@gmail.com>
2019-05-09 17:15:24 -0400

    randomize the input offsets in bench_multiprocess
    Also switch to measuring the median time, and do a better job of
    calibrating the run length.

--- benches/bench_multiprocess/src/main.rs
+++ benches/bench_multiprocess/src/main.rs
@@ -9,6 +9,7 @@
 
 extern crate blake2b_simd;
 
+use rand::seq::SliceRandom;
 use rand::RngCore;
 use std::cmp::Ordering::{Greater, Less};
 use std::env;
@@ -33,7 +34,7 @@ const CALIBRATION_INPUT_LEN: usize = 1 << 20;
 static ALGOS: &[(&str, HashFn)] = &[
     ("blake2b_simd portable", hash_portable),
     ("blake2b_simd AVX2", hash_avx2),
-    ("blake2b_simd hash_many", hash_many),
+    ("blake2b_simd many", hash_many),
     ("blake2b_simd BLAKE2bp", hash_blake2bp),
     ("blake2b_avx2_neves BLAKE2bp", hash_neves_blake2bp),
     ("libsodium BLAKE2b", libsodium),
@@ -41,98 +42,126 @@ static ALGOS: &[(&str, HashFn)] = &[
     ("OpenSSL SHA-512", openssl_sha512),
 ];
 
-fn hash_portable(input: &[u8]) {
+fn hash_portable(input: &mut RandomInput) {
     let mut state = blake2b_simd::State::new();
     blake2b_simd::benchmarks::force_portable(&mut state);
-    state.update(input);
+    state.update(input.get());
     state.finalize();
 }
 
-fn hash_avx2(input: &[u8]) {
-    blake2b_simd::blake2b(input);
+fn hash_avx2(input: &mut RandomInput) {
+    blake2b_simd::blake2b(input.get());
 }
 
-fn hash_many(input: &[u8]) {
-    // Note that the caller makes sure these divisions are going to coincide
-    // with page boundaries, and we assert that again here.
-    let degree = blake2b_simd::many::degree();
-    let page_size = page_size::get();
-    assert_eq!(0, input.len() % (degree * page_size));
-    let part_len = input.len() / degree;
+fn hash_many(input: &mut RandomInput) {
+    let mut input_slices = arrayvec::ArrayVec::<[&[u8]; blake2b_simd::many::MAX_DEGREE]>::new();
+    for _ in 0..blake2b_simd::many::degree() {
+        input_slices.push(&[]);
+    }
+    input.get_n(&mut input_slices);
 
     let params = blake2b_simd::Params::new();
     let mut jobs = arrayvec::ArrayVec::<[_; blake2b_simd::many::MAX_DEGREE]>::new();
-    for i in 0..degree {
-        let part = &input[i * part_len..][..part_len];
-        let job = blake2b_simd::many::HashManyJob::new(&params, part);
+    for &input_slice in &input_slices {
+        let job = blake2b_simd::many::HashManyJob::new(&params, input_slice);
         jobs.push(job);
     }
 
     blake2b_simd::many::hash_many(&mut jobs);
 }
 
-fn hash_blake2bp(input: &[u8]) {
-    blake2b_simd::blake2bp::blake2bp(input);
+fn hash_blake2bp(input: &mut RandomInput) {
+    blake2b_simd::blake2bp::blake2bp(input.get());
 }
 
-fn hash_neves_blake2bp(input: &[u8]) {
-    blake2_avx2_neves::blake2bp(input);
+fn hash_neves_blake2bp(input: &mut RandomInput) {
+    blake2_avx2_neves::blake2bp(input.get());
 }
 
-fn libsodium(input: &[u8]) {
+fn libsodium(input: &mut RandomInput) {
     let mut out = [0; 64];
     unsafe {
         let init_ret = libsodium_ffi::sodium_init();
         assert!(init_ret != -1);
     }
+    let input_slice = input.get();
     unsafe {
         libsodium_ffi::crypto_generichash(
             out.as_mut_ptr(),
             out.len(),
-            input.as_ptr(),
-            input.len() as u64,
+            input_slice.as_ptr(),
+            input_slice.len() as u64,
             ptr::null(),
             0,
         );
     };
 }
 
-fn openssl_sha1(input: &[u8]) {
-    openssl::hash::hash(openssl::hash::MessageDigest::sha1(), &input).unwrap();
+fn openssl_sha1(input: &mut RandomInput) {
+    openssl::hash::hash(openssl::hash::MessageDigest::sha1(), input.get()).unwrap();
 }
 
-fn openssl_sha512(input: &[u8]) {
-    openssl::hash::hash(openssl::hash::MessageDigest::sha512(), &input).unwrap();
+fn openssl_sha512(input: &mut RandomInput) {
+    openssl::hash::hash(openssl::hash::MessageDigest::sha512(), input.get()).unwrap();
 }
 
-type HashFn = fn(input: &[u8]);
+type HashFn = fn(input: &mut RandomInput);
 
-// Do two special things to the input:
-// 1. Fill it with random bytes. This probably isn't strictly necessary, but
-//    it's important that it's written to with *something*, because I believe
-//    asking the allocator for pages of all zeroes hits special copy-on-write
-//    optimizations in the kernel that we want to avoid.
-// 2. Make sure that the input is aligned to a page boundary. WORKER_OFFSET is
-//    interpreted relative to that alignment, so an offset of zero means the
-//    input starts exactly at a the page boundary. This seems especially
-//    important for the performance of BLAKE2bp, though I'm not sure exactly
-//    why. Exact page alignment seems to give reasonably good performance
-//    compared to most other offsets (though not strictly the best, see
-//    https://github.com/oconnor663/blake2b_simd/issues/8), and it seems like a
-//    "fair" benchmarking point.
-fn make_input(vec: &mut Vec<u8>, len: usize) -> &[u8] {
-    let input_offset: usize = if let Ok(offset) = env::var("WORKER_OFFSET") {
-        offset.parse().unwrap()
-    } else {
-        0
-    };
-    let page_size: usize = page_size::get();
-    *vec = vec![0; len + page_size + input_offset];
-    rand::thread_rng().fill_bytes(vec);
-    let allocated_offset = vec.as_ptr() as usize % page_size;
-    let next_page_start = page_size - allocated_offset;
-    let target_offset = next_page_start + input_offset;
-    &vec[target_offset..][..len]
+struct VecIter<T> {
+    vec: Vec<T>,
+    i: usize,
+}
+
+impl<T: Clone> VecIter<T> {
+    fn next(&mut self) -> T {
+        let item = self.vec[self.i].clone();
+        self.i += 1;
+        if self.i >= self.vec.len() {
+            self.i = 0;
+        }
+        item
+    }
+}
+
+// This struct randomizes two things:
+// 1. The actual bytes of input.
+// 2. The page offset the input starts at.
+struct RandomInput {
+    buf: Vec<u8>,
+    len: usize,
+    offsets: VecIter<usize>,
+}
+
+impl RandomInput {
+    fn new(len: usize) -> Self {
+        let page_size: usize = page_size::get();
+        let mut buf = vec![0u8; len + page_size];
+        rand::thread_rng().fill_bytes(&mut buf);
+        let mut offsets: Vec<usize> = (0..page_size).collect();
+        offsets.shuffle(&mut rand::thread_rng());
+        Self {
+            buf,
+            len,
+            offsets: VecIter { vec: offsets, i: 0 },
+        }
+    }
+
+    fn get(&mut self) -> &[u8] {
+        &self.buf[self.offsets.next()..][..self.len]
+    }
+
+    // for hash_many
+    #[inline]
+    fn get_n<'a>(&'a mut self, out: &mut [&'a [u8]]) {
+        let n = out.len();
+        let slice_len = self.len / n;
+        let mut pos = 0;
+        for slice in out {
+            pos += self.offsets.next() / n;
+            *slice = &self.buf[pos..][..slice_len];
+            pos += slice_len;
+        }
+    }
 }
 
 fn time_ns<F: FnOnce()>(f: F) -> u128 {
@@ -158,7 +187,7 @@ fn rate_f32(ns: u128, input_len: usize) -> f32 {
     input_len as f32 / ns as f32
 }
 
-fn ns_per_run() -> u128 {
+fn ns_per_worker() -> u128 {
     let ms_per_bench: u128 = if let Some(secs) = env::var_os("MS_PER_BENCH") {
         let ms_str = secs.to_str().expect("unicode");
         if let Ok(ms) = ms_str.trim().parse() {
@@ -170,25 +199,20 @@ fn ns_per_run() -> u128 {
         DEFAULT_MS_PER_BENCH
     };
     let ns_per_bench = 1_000_000 * ms_per_bench;
-    let total_runs_with_warmup = WORKERS * (RUNS_PER_WORKER + 1);
-    ns_per_bench / total_runs_with_warmup as u128
+    ns_per_bench / WORKERS as u128
 }
 
 fn worker(algo: &str) {
     let hash_fn = get_hash_fn(algo);
     let input_len: usize = env::var("WORKER_LEN").unwrap().parse().unwrap();
-    let mut input_vec = Vec::new();
-    let input = make_input(&mut input_vec, input_len);
+    let mut input = RandomInput::new(input_len);
 
     // Do a dummy run to warm up.
-    hash_fn(&input);
+    hash_fn(&mut input);
 
     let mut total_ns = 0;
     for _ in 0..RUNS_PER_WORKER {
-        let before = Instant::now();
-        hash_fn(&input);
-        let after = Instant::now();
-        let ns = (after - before).as_nanos();
+        let ns = time_ns(|| hash_fn(&mut input));
         // eprintln!("run throughput: {}", rate_f32(ns, input_len));
         total_ns += ns;
     }
@@ -200,21 +224,17 @@ fn run_algo(algo_name: &str) -> f32 {
     // that to figure out how input to give each worker. Note that it's
     // important to do this in the main process, because doing it
     // individually in each worker would give more input to slower workers.
-    let mut input_vec = Vec::new();
-    let test_input = make_input(&mut input_vec, CALIBRATION_INPUT_LEN);
     let hash_fn = get_hash_fn(algo_name);
-    hash_fn(&test_input); // warm-up calibration run
-    let test_ns = time_ns(|| hash_fn(&test_input));
+    let test_ns = time_ns(|| {
+        let mut test_input = RandomInput::new(CALIBRATION_INPUT_LEN);
+        hash_fn(&mut test_input); // dummy warm up run
+        for _ in 0..RUNS_PER_WORKER {
+            hash_fn(&mut test_input);
+        }
+    });
 
-    // Given the test time found above, compute the worker input length. The
-    // hash_many implementation is going to divide this input up, so make sure
-    // those divisions will occur on page boundaries too, for runtime
-    // stability. Note that this seems to *hurt* the measured performance by 1%
-    // or so, and I'm not sure why. See
-    // https://github.com/oconnor663/blake2b_simd/issues/8.
-    let mut worker_len = (CALIBRATION_INPUT_LEN as u128 * ns_per_run() / test_ns) as usize;
-    let block = blake2b_simd::many::degree() * page_size::get();
-    worker_len += block - (worker_len % block);
+    // Given the test time found above, compute the worker input length.
+    let worker_len = (CALIBRATION_INPUT_LEN as u128 * ns_per_worker() / test_ns) as usize;
 
     // Fire off all the workers in series and collect their reported times.
     let mut times = Vec::new();
@@ -222,9 +242,6 @@ fn run_algo(algo_name: &str) -> f32 {
     for _ in 0..WORKERS {
         env::set_var("BENCH_ALGO", algo_name);
         env::set_var("WORKER_LEN", worker_len.to_string());
-        if env::var_os("WORKER_OFFSET").is_none() {
-            env::set_var("WORKER_OFFSET", "0");
-        }
         let mut cmd = process::Command::new(env::current_exe().unwrap());
         cmd.stdout(process::Stdio::piped());
         let child = cmd.spawn().unwrap();
@@ -242,8 +259,8 @@ fn run_algo(algo_name: &str) -> f32 {
         _total_ns += ns;
     }
     times.sort();
-    let _min_time = times[0];
-    let throughput = rate_f32(_min_time, RUNS_PER_WORKER * worker_len);
+    let median_time = times[times.len() / 2];
+    let throughput = rate_f32(median_time, RUNS_PER_WORKER * worker_len);
 
     // an alternative that uses the average instead of the minimum
     // let throughput = rate_f32(total_ns, WORKERS * RUNS_PER_WORKER * worker_len);
