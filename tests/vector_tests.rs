ab8aef8bfbef616ac8c9d3232cb7e7139fa59bff
Jack O'Connor <oconnor663@gmail.com>
2019-05-18 11:52:30 -0400

    add blake2s to the shared tests -- vectors start failing

--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -85,3 +85,67 @@ fn blake2bp_vectors() {
     // count.
     assert_eq!(512, test_num);
 }
+
+#[test]
+fn blake2s_vectors() {
+    let mut test_num = 0u64;
+    for case in TEST_CASES.iter() {
+        if &case.hash == "blake2s" {
+            test_num += 1;
+            println!("input {:?}, key {:?}", case.in_, case.key);
+
+            let input_bytes = hex::decode(&case.in_).unwrap();
+            let mut params = blake2s_simd::Params::new();
+            if !case.key.is_empty() {
+                let key_bytes = hex::decode(&case.key).unwrap();
+                params.key(&key_bytes);
+            }
+
+            // Assert the all-at-once result.
+            assert_eq!(case.out, &*params.hash(&input_bytes).to_hex());
+
+            // Assert the State result.
+            let mut state = params.to_state();
+            state.update(&input_bytes);
+            assert_eq!(case.out, &*state.finalize().to_hex());
+            assert_eq!(input_bytes.len() as u128, state.count());
+        }
+    }
+
+    // Make sure we don't accidentally skip all the tests somehow. If the
+    // number of test vectors changes in the future, we'll need to update this
+    // count.
+    assert_eq!(512, test_num);
+}
+
+#[test]
+fn blake2sp_vectors() {
+    let mut test_num = 0u64;
+    for case in TEST_CASES.iter() {
+        if &case.hash == "blake2sp" {
+            test_num += 1;
+            println!("input {:?}, key {:?}", case.in_, case.key);
+
+            let input_bytes = hex::decode(&case.in_).unwrap();
+            let mut params = blake2s_simd::blake2sp::Params::new();
+            if !case.key.is_empty() {
+                let key_bytes = hex::decode(&case.key).unwrap();
+                params.key(&key_bytes);
+            }
+
+            // Assert the all-at-once result.
+            assert_eq!(case.out, &*params.hash(&input_bytes).to_hex());
+
+            // Assert the State result.
+            let mut state = params.to_state();
+            state.update(&input_bytes);
+            assert_eq!(case.out, &*state.finalize().to_hex());
+            assert_eq!(input_bytes.len() as u128, state.count());
+        }
+    }
+
+    // Make sure we don't accidentally skip all the tests somehow. If the
+    // number of test vectors changes in the future, we'll need to update this
+    // count.
+    assert_eq!(512, test_num);
+}
