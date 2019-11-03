101caf59fd79b45b37ad0fb6014c8071d0db763a
Jack O'Connor <oconnor663@gmail.com>
2019-11-02 23:25:46 -0400

    enable K12 benchmarks on non-x86

--- benches/kangarootwelve/build.rs
+++ benches/kangarootwelve/build.rs
@@ -2,23 +2,35 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
+fn target_name() -> &'static str {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        if is_x86_feature_detected!("avx512f") {
+            assert!(
+                is_x86_feature_detected!("avx512vl"),
+                "AVX-512F supported but not AVX-512VL. This isn't SkylakeX.",
+            );
+            return "SkylakeX";
+        } else if is_x86_feature_detected!("avx2") {
+            return "Haswell";
+        }
+    }
+    if std::mem::size_of::<usize>() == 8 {
+        "generic64"
+    } else if std::mem::size_of::<usize>() == 4 {
+        "generic32"
+    } else {
+        panic!("unexpected word size {}", std::mem::size_of::<usize>())
+    }
+}
+
 fn main() {
     // Do local CPU feature detection to determine which version to build. This
     // wouldn't be correct if we were building a distributable binary, because
     // the features of the target machine wouldn't necessarily be the same as
     // the features of the build machine. But this build is just for
     // benchmarks, so it's fine.
-    let target = if is_x86_feature_detected!("avx512f") {
-        assert!(
-            is_x86_feature_detected!("avx512vl"),
-            "AVX-512F supported but not AVX-512VL. This isn't SkylakeX.",
-        );
-        "SkylakeX"
-    } else if is_x86_feature_detected!("avx2") {
-        "Haswell"
-    } else {
-        panic!("this benchmark requires at least AVX2")
-    };
+    let target = target_name();
     let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
     let k12_dir = manifest_dir.join("K12");
     let build_dir = k12_dir.join(format!("bin/{}", target));
