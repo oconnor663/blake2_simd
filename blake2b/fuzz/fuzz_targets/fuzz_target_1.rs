30bbdfeb1003816a5b04b45fdbeeb64ef7df7b99
Kirk Baird <baird.k@outlook.com>
2020-04-14 18:18:35 +1000

    Add fuzz target which fails compilation
    Signed-off-by: Kirk Baird <baird.k@outlook.com>

---
+++ blake2b/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,24 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use blake2b_simd::{blake2b, Params};
+
+fuzz_target!(|data: &[u8]| {
+let expected = "ca002330e69d3e6b84a46a56a6533fd79d51d97a3bb7cad6c2ff43b354185d6d\
+                c1e723fb3db4ae0737e120378424c714bb982d9dc5bbd7a0ab318240ddd18f8d";
+let hash = blake2b(b"foo");
+assert_eq!(expected, &hash.to_hex());
+
+let hash = Params::new()
+    .hash_length(16)
+    .key(b"The Magic Words are Squeamish Ossifrage")
+    .personal(b"L. P. Waterhouse")
+    .to_state()
+    .update(b"foo")
+    .update(b"bar")
+    .update(b"baz")
+    .finalize();
+assert_eq!("ee8ff4e9be887297cf79348dc35dab56", &hash.to_hex());
+
+    // fuzzed code goes here
+});
